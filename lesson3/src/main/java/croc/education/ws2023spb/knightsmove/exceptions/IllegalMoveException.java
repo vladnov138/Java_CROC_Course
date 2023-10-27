2dcc31caef6b839db3b6c95674d135a70a9f731f
vladnov138 <vladnov138@gmail.com>
2023-10-27 19:58:20 +0800

    move, add docs

--- lesson3/src/main/java/croc/education/ws2023spb/knightsmove/exceptions/IllegalMoveException.java
+++ lesson3/src/main/java/croc/education/ws2023spb/knightsmove/exceptions/IllegalMoveException.java
@@ -1,4 +1,4 @@
-package croc.education.ws2023spb.knightsmove;
+package croc.education.ws2023spb.knightsmove.exceptions;
 
 /**
  * Исключение, выбрасываемое в случае, если при перемещении шахматного коня из текущей клетки в следующую происходит с
@@ -24,15 +24,20 @@ public class IllegalMoveException extends Exception {
     }
 
     /**
+     * Конструктор с передачей сообщения и причины, вызвавшей это исключение.
      * @param message
+     *          сообщение об ошибке
      * @param cause
+     *          исключение, которое послужило причиной вызова исключения
      */
     public IllegalMoveException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /**
+     * Конструктор с передачей причины, вызвавшей это исключение.
      * @param cause
+     *          исключение, которое послужило причиной вызова исключения
      */
     public IllegalMoveException(Throwable cause) {
         super(cause);
--- lesson3/src/main/java/croc/education/ws2023spb/knightsmove/exceptions/IllegalPositionException.java
+++ lesson3/src/main/java/croc/education/ws2023spb/knightsmove/exceptions/IllegalPositionException.java
@@ -1,4 +1,4 @@
-package croc.education.ws2023spb.knightsmove;
+package croc.education.ws2023spb.knightsmove.exceptions;
 
 /**
  * Исключение, выбрасываемое в случае, если указана неправильная шахматная координата
@@ -23,15 +23,20 @@ public class IllegalPositionException extends Exception {
     }
 
     /**
+     * Конструктор с передачей сообщения и причины, вызвавшей это исключение.
      * @param message
+     *          сообщение об ошибке
      * @param cause
+     *          исключение, которое послужило причиной вызова исключения
      */
     public IllegalPositionException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /**
+     * Конструктор с передачей причины, вызвавшей это исключение.
      * @param cause
+     *          исключение, которое послужило причиной вызова исключения
      */
     public IllegalPositionException(Throwable cause) {
         super(cause);
