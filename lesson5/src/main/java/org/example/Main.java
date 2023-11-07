a3a6f23adcedaffae66c5b3abf00be31cec30599
vladnov138 <vladnov138@gmail.com>
2023-11-07 21:51:07 +0800

    Create lesson5

---
+++ lesson5/src/main/java/org/example/Main.java
@@ -0,0 +1,126 @@
+package org.example;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+
+/**
+ * Стартовый класс.
+ */
+public class Main {
+
+    // Объявляем ExecutorService с максимально возможным числом потоков.
+    private static final ExecutorService executorService =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    /**
+     * Точка старта приложения
+     *
+     * @param args стартовые аргументы
+     */
+    public static void main(String[] args) {
+
+        var matrix = MatrixExample.E;
+
+        printResult("detMultiThread", matrix, () -> {
+            try {
+                return detMultiThread(matrix.getMatrix());
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        printResult("detOneThread", matrix, () -> detOneThread(matrix.getMatrix()));
+        executorService.shutdown();
+    }
+
+    /**
+     * Рекурсивный расчет определителя матрицы методом разложения по строке в один поток.
+     *
+     * @param a матрица
+     * @return определитель матрицы
+     */
+    public static long detOneThread(long[][] a) {
+        if (a.length == 1) {
+            return a[0][0];
+        }
+        var result = 0L;
+        for (var i = 0; i < a.length; i++) {
+            var sign = (i % 2 == 0 ? 1 : -1);
+            result = result + sign * a[i][0] * detOneThread(minor(a, i));
+        }
+        return result;
+    }
+
+    /**
+     * Рекурсивный расчет определителя матрицы методом разложения по строке через многопоточность
+     * (число потоков = a.length)
+     *
+     * @param a матрица
+     * @return определитель матрицы
+     * @throws ExecutionException   если вычисление было прервано или завершилось с ошибкой
+     * @throws InterruptedException если текущий поток был прерван, пока ожидал завершения вычисления
+     */
+    public static long detMultiThread(long[][] a) throws ExecutionException, InterruptedException {
+        if (a.length == 1) {
+            return 1;
+        }
+        Future<Long>[] futures = new Future[a.length];
+        for (var i = 0; i < a.length; i++) {
+            var sign = (i % 2 == 0 ? 1 : -1);
+            final int finalI = i;
+            // Вот тут если вставить detMultiThread, то можно уходить пить чай, т.к. будет ооочень долго считать.
+            // Предполагаю, что связано это с тем, что он насоздавал потоки больше возможного кол-ва, и те, видимо,
+            // ждут своей очереди.
+            futures[i] = executorService.submit(() -> sign * a[finalI][0] * detOneThread(minor(a, finalI)));
+        }
+        var result = new AtomicLong(0L); // для безопасности
+        // Нам необходимо все записать в массив или лист с типом Future<Long>. Иначе будет блокироваться поток в
+        // ожидании результата.
+        for (Future<Long> future :
+                futures) {
+            result.addAndGet(future.get());
+        }
+        return result.get();
+    }
+
+    /**
+     * Вычисляет минорную матрицу от заданной. Удаляется первый столбец и заданная строка.
+     *
+     * @param original  матрица, от которой требуется вычислить минорную
+     * @param exceptRow удаляемая строка
+     * @return минорная матрица
+     */
+    public static long[][] minor(final long[][] original, int exceptRow) {
+        long[][] minor = new long[original.length - 1][original.length - 1];
+        var minorLength = minor.length;
+        for (int i = 0; i < exceptRow; i++) {
+            System.arraycopy(original[i], 1, minor[i], 0, minorLength);
+        }
+        for (int i = exceptRow + 1; i < original.length; i++) {
+            System.arraycopy(original[i], 1, minor[i - 1], 0, minorLength);
+        }
+        return minor;
+    }
+
+    /**
+     * Выводит в консоль результат работы.
+     *
+     * @param method   название метода расчета
+     * @param matrix   матрица из предложенных для примера
+     * @param executor алгоритм расчета определителя матрицы
+     */
+    private static void printResult(String method, MatrixExample matrix, Supplier<Long> executor) {
+        var start = System.currentTimeMillis();
+        var det = executor.get();
+        var executionTime = (System.currentTimeMillis() - start);
+        System.out.println("Method -> " + method);
+        System.out.println("Matrix name -> " + matrix.name());
+        System.out.println("Matrix dimension -> " + matrix.getMatrix().length);
+        System.out.println("Matrix determinant  = " + det + (det != matrix.getDeterminant() ? " ERROR!" : ""));
+        System.out.println("Execution time -> " + executionTime);
+        System.out.println();
+    }
+}
---
+++ lesson5/src/test/java/TestMain.java
@@ -0,0 +1,50 @@
+import org.example.Main;
+import org.example.MatrixExample;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestMain {
+    /**
+     * Тест метода {@link Main#detOneThread(long[][])}
+     * <p/>
+     * Проверяет правильность подсчета определителя матрицы в одном потоке
+     */
+    @Test
+    public void testDetOneThread() {
+        // given:
+        var matrixExample = MatrixExample.C;
+        var matrix = matrixExample.getMatrix();
+        long answer = matrixExample.getDeterminant();
+
+        // when:
+        long result = Main.detOneThread(matrix);
+        
+        // then:
+        assertThat(result).isEqualTo(answer);
+    }
+
+    /**
+     * Тест метода {@link Main#detMultiThread(long[][])}
+     * <p/>
+     * Проверяет правильность подсчета определителя матрицы в многопоточном режиме
+     *
+     * @throws ExecutionException   при ошибке теста
+     * @throws InterruptedException при ошибке теста
+     */
+    @Test
+    public void testDetMultiThread() throws ExecutionException, InterruptedException {
+        // given:
+        var matrixExample = MatrixExample.C;
+        var matrix = matrixExample.getMatrix();
+        long answer = matrixExample.getDeterminant();
+
+        // when:
+        long result = Main.detMultiThread(matrix);
+
+        // then:
+        assertThat(result).isEqualTo(answer);
+    }
+}
